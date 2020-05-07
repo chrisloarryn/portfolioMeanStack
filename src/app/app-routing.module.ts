345a409f3b3ba173a858fcc64470597bce9f8691
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-07 01:00:45 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home';
+import { ContactComponent } from './components/contact';
+import { ViewProjectComponent } from './components/view-project';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
+  // ,
+  // {
+  //   path: 'contact',
+  //   component: ContactComponent
+  // },
+  // {
+  //   path: 'projects/:id',
+  //   component: ViewProjectComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
